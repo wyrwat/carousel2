f5937983f6b5e4a4def98f9954d221196f2086c5
tomasz wyrwa <wyrwa.t@gmail.com>
2019-04-20 12:46:20 +0200

    add button

--- js/script.js
+++ js/script.js
@@ -6,21 +6,10 @@ var flkty = new Flickity( elem, {
   pageDots: false
 });
 
-
-
-
-
-/*var restart = new Flickity('.carousel');
-
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
 buttons = fizzyUIUtils.makeArray( buttons );
 
 buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  restart.select( index );
-});*/
+  flkty.select(0);
+});
